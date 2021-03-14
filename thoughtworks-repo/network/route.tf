165562b5f34a7adb8966a427f3e6aaa77a2017c8
root <root@1f9f1c5b1b1c.mylabserver.com>
2021-03-14 21:03:16 +0000

    up-to-date

--- thoughtworks-repo/network/route.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_zone" "main" {
-  count = var.create_route53_zone ? 1 : 0 
-  name  = var.zone_name
-  vpc {
-    vpc_id = concat(aws_vpc.this.*.id, [""])[0]
-  }
-
-  tags = var.tags
-}
--- thoughtworks-repo/r53/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "hostname" {
-  value       = join("", aws_route53_record.www.*.fqdn)
-  description = "DNS hostname"
-}
